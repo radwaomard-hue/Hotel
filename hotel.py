776366075235726bf5088c942fb96566a6fdbad2
radwaomard-hue <radwaomard@gmail.com>
2026-09-09 18:11:09 +0300

    Fix invalid number input
    Added input validation for name, age, address, and nationality to ensure valid entries from users. Updated total calculation for meals based on user choices.

--- hotel.py
+++ hotel.py
@@ -4,10 +4,49 @@ print("\n" + "=" * 50)
 print("                    HOTEL")
 print("=" * 50)
 
-name = input("Enter your name: ").strip()
-age = int(input("Enter your age: "))
-address = input("Enter your address: ").strip()
-nationality = input("Enter your nationality: ").strip()
+
+while True:
+	name = input("\n Enter your name: ").strip()
+	
+	if name.isalpha():
+		print(name)
+		break
+	else:
+		print("Invalid name")
+		
+		
+while True:
+    age = input("\nEnter your age: ")
+
+    if age.isdigit():
+        age = int(age)
+        print(age)
+        break
+    else:
+        print("Invalid age")
+
+		
+while True:
+	address = input("\nEnter your address: ").strip()
+	
+	if address.isalpha():
+	   print(address)
+	   break
+	else:
+		print("Invalid address")
+	
+
+while True:
+	nationality = input("\nEnter your nationality: ").strip()
+	
+	if nationality.isalpha():
+		print(nationality)
+		break
+		
+	else:
+		print('invalid nationality')
+		
+	
 
 rooms = {
     "room101": 550,
@@ -36,6 +75,67 @@ else:
     else:
         print("Invalid booking type.")
         exit()
+
+
+print("\n" + "=" * 50)
+print("                    RESTAURANT")
+print("=" * 50)
+breakfast = {
+    "pie": 20,
+    "cheese": 15,
+    "beans": 10
+}
+
+lunch = {
+    "pizza": 200,
+    "pasta": 150,
+    "burger": 100
+}
+
+restaurant = input("Do you want breakfast or lunch? (yes/no): ").strip().lower()
+
+if restaurant == "yes":
+    meal = input("Do you want breakfast or lunch? ").strip().lower()
+
+    if meal == "breakfast":
+        print("\nAvailable breakfast:")
+
+        for food, price in breakfast.items():
+            print(f"{food} - ${price} per day")
+
+        choice_meal = input("\nChoose your breakfast: ").strip().lower()
+
+        if choice_meal in breakfast:
+            total2 = total + breakfast[choice_meal]
+            
+        else:
+            print("Invalid breakfast choice")
+
+    elif meal == "lunch":
+        print("\nAvailable lunch:")
+
+        for food, price in lunch.items():
+            print(f"{food} - ${price} per day")
+
+        choice_meal = input("\nChoose your lunch: ").strip().lower()
+
+        if choice_meal in lunch:
+            total2 = total + lunch[choice_meal]
+        
+        else:
+            print("Invalid lunch choice")
+
+    else:
+        print("Invalid meal choice")
+
+elif restaurant == "no":
+    print("Okay")
+    total2 = total
+
+else:
+    print("No answer")
+
+    
     now = datetime.now()
     print("\n" + "=" * 50)
     print("                CUSTOMER INFORMATION")
@@ -46,7 +146,7 @@ else:
     print(f"Nationality : {nationality}")
     print(f"Room        : {choice}")
     print(f"Booking     : {booking_type}")
-    print(f"Total       : ${total:.2f}")
+    print(f"Total       : ${total2:.2f}")
     print(f"Date        : {now.date()}")
     print(f"Time        : {now.strftime('%H:%M:%S')}")
 
